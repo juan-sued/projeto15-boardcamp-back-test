729797a4c99eadfb463e02288cae746ceac07143
juansued1233536 <92616927+juansued1233536@users.noreply.github.com>
2022-07-25 15:35:07 -0300

    adicionada função de pegar rentals

--- src/controllers/rentalsController.js
+++ src/controllers/rentalsController.js
@@ -22,3 +22,52 @@ export async function registerRental(request, response) {
     return response.status(500).send('erro ao adicionar rental no banco');
   }
 }
+
+export async function getRentals(request, response) {
+  const { customerId } = request.query;
+  const { gameId } = request.query;
+
+  const queryBasic = `SELECT rentals.*, customers.name AS "customerName", games.name AS "gameName", categories.name AS "categoryName", categories.id AS "categoryId" FROM rentals JOIN customers ON rentals."customerId" = customers.id JOIN games ON rentals."gameId" = games.id JOIN categories ON games."categoryId" = categories.id;`;
+  let query = queryBasic;
+
+  try {
+    if (!!customerId) {
+      query = queryBasic + ` WHERE customers.id = '${customerId}';`;
+    } else if (!!gameId) {
+      query = queryBasic + ` WHERE games.id = '${gameId}';`;
+    }
+
+    const { rows: rentals } = await connection.query(query);
+
+    const rentalsJoin = [];
+
+    for (let rental of rentals) {
+      rentalsJoin.push({
+        id: rental.id,
+        customerId: rental.customerId,
+        gameId: rental.gameId,
+        rentDate: rental.rentDate,
+        daysRented: rental.daysRented,
+        returnDate: rental.returnDate,
+        originalPrice: rental.originalPrice,
+        delayFee: rental.delayFee,
+        customer: {
+          id: rental.customerId,
+          name: rental.customerName
+        },
+        game: {
+          id: rental.gameId,
+          name: rental.gameName,
+          categoryId: rental.categoryId,
+          categoryName: rental.categoryName
+        }
+      });
+    }
+
+    if (!rentals) return response.sendStatus(404);
+
+    return response.status(200).send(rentalsJoin);
+  } catch {
+    return response.status(500).send('erro ao pegar alugueis');
+  }
+}
